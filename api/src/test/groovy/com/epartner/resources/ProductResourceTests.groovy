70a0d072d137afb4872b58a4c8e7c3dc4c2d0d20
Maxi Britez <maxi.britez@redb.ee>
2016-09-03 19:12:49 -0300

    Add product resource tests

---
+++ api/src/test/groovy/com/epartner/resources/ProductResourceTests.groovy
@@ -0,0 +1,73 @@
+package com.epartner.resources
+
+import com.epartner.representations.ProductRepresentation
+import com.epartner.services.ProductService
+import org.springframework.data.domain.PageImpl
+import spock.lang.Specification
+
+/**
+ * Created by mbritez on 3/09/16.
+ */
+class ProductResourceTests extends Specification {
+
+    private ProductResource resource
+    private ProductService mockedService
+
+    def setup(){
+        mockedService = Mock(ProductService)
+        resource = new ProductResource(mockedService)
+    }
+
+    def "test create"(){
+        setup:
+        def mockedRepresentation = Mock(ProductRepresentation)
+        when:
+        def result = resource.create(mockedRepresentation)
+        then:
+        1 * mockedService.create(mockedRepresentation) >> mockedRepresentation
+        result
+        result == mockedRepresentation
+    }
+
+    def "test update"() {
+        setup:
+        def mockedRepresentation = Mock(ProductRepresentation)
+        when:
+        def result = resource.update(mockedRepresentation, 1L)
+        then:
+        1 * mockedService.update(mockedRepresentation, 1L) >> mockedRepresentation
+        result
+        result == mockedRepresentation
+    }
+
+    def "test delete"() {
+        when:
+        resource.delete(1L)
+        then:
+        1 * mockedService.delete(1L)
+    }
+
+    def "test get"(){
+        setup:
+        def mockedRepresentation = Mock(ProductRepresentation)
+        when:
+        def result = resource.get(1L)
+        then:
+        1 * mockedService.show(1L) >> mockedRepresentation
+        result
+    }
+
+    def "test list"() {
+        setup:
+        def mockedProduct = Mock(ProductRepresentation)
+        def mockedResult = new PageImpl([mockedProduct])
+        when:
+        def result = resource.list(10, 0)
+        then:
+        1 * mockedService.list(Optional.of(10), Optional.of(0)) >> mockedResult
+        result
+        def product = result.getContent().get(0)
+        product
+        product == mockedProduct
+    }
+}
