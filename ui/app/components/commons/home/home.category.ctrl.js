c55e73fe64d13efe55f126c95c1c61ba2171b2e8
Martin Britez <martin.britez@redb.ee>
2016-09-25 21:23:37 -0300

    productoDestacado

--- ui/app/components/commons/home/home.category.ctrl.js
+++ ui/app/components/commons/home/home.category.ctrl.js
@@ -1,8 +1,37 @@
 'use strict';
 
-export default class HomeCategoryCtrl{
+export default class HomeCategoryCtrl {
 
     /*@ngInject*/
     constructor() {
+        this.init();
+
+    }
+
+    init() {
+        this.entity = {};
+        this.entity.destacados = [{
+            
+            title: 'SMART TV Led 32',
+            price: '4000',
+            image: 'http://mla-s1-p.mlstatic.com/785221-MLA20748837164_062016-O.jpg'
+        },
+        {
+            title: 'Baby Call',
+            price: '299',
+            image: 'http://mla-s2-p.mlstatic.com/313121-MLA20717004267_052016-O.jpg'
+        },
+        {
+            title: 'Masajeador',
+            price: '560',
+            image: 'http://mla-s2-p.mlstatic.com/754101-MLA20278315510_042015-O.jpg'
+        },
+        {
+            title: 'Otro TV',
+            price: '40077',
+            image: 'http://mla-s1-p.mlstatic.com/504711-MLA20609465620_022016-O.jpg'
+        }
+        ];
+      
     }
 }
