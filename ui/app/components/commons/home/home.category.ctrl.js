b209e05f3996b2f8d6af387f800c2c7c71e977de
Martin Britez <martin.britez@redb.ee>
2016-10-28 10:16:19 -0300

    productTagDetail

--- ui/app/components/commons/home/home.category.ctrl.js
+++ ui/app/components/commons/home/home.category.ctrl.js
@@ -3,35 +3,22 @@
 export default class HomeCategoryCtrl {
 
     /*@ngInject*/
-    constructor() {
-        this.init();
+    constructor(api,$stateParams) {
+        this.stateParams = $stateParams;
+        this.api = api;
+        this.init()
 
     }
 
+
     init() {
-        this.entity = {};
-        this.entity.destacados = [{
-            
-            title: 'SMART TV Led 32',
-            price: '4000',
-            image: 'http://mla-s1-p.mlstatic.com/785221-MLA20748837164_062016-O.jpg'
-        },
-        {
-            title: 'Baby Call',
-            price: '299',
-            image: 'http://mla-s2-p.mlstatic.com/313121-MLA20717004267_052016-O.jpg'
-        },
-        {
-            title: 'Masajeador',
-            price: '560',
-            image: 'http://mla-s2-p.mlstatic.com/754101-MLA20278315510_042015-O.jpg'
-        },
-        {
-            title: 'Otro TV',
-            price: '40077',
-            image: 'http://mla-s1-p.mlstatic.com/504711-MLA20609465620_022016-O.jpg'
-        }
-        ];
-      
+        this.api
+            .tags
+            .get({id: this.stateParams.id})
+            .$promise
+            .then(response => {
+                this.entity = response;
+                this.entity.name = this.entity.name;
+            });
     }
 }
