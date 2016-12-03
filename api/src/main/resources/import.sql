9ba9d9065a5b4a4c1113ca7c05e6ebb724c55704
Matías Gonzalez <matias@tranzeal.com>
2016-12-03 13:00:42 -0800

    se agrega script para creacion de categorias

---
+++ api/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+insert into category(name, description, highlight) values('telefono','telefonia & demas',1);
+insert into category(name, description, highlight) values('televisores','altas teles capo',1);
+insert into category(name, description, highlight) values('electrodomesticos','para la casa',0);
+insert into category(name, description, highlight) values('cuidado personal','telefonia & demas',0);
+
+
+/*
+insert into product(name, description, stok, price, isImported, isPublished, category_id)
+values('Samsung 40','teles', 10,10000,0,1,2);
+
+insert into product(name, description, stok, price, isImported, isPublished, category_id)
+values('iPhone 7','cels', 10,20000,0,1,1);
+
+insert into product(name, description, stok, price, isImported, isPublished, category_id)
+values('iPhone 8','cels', 10,150000,0,1,0);*/
