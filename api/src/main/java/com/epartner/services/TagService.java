d83a92e37fd263cdf9f7cb38a50d5a4248d00868
Maxi <maxi.britez@redb.ee>
2017-01-31 10:56:40 -0300

    Fix tags

--- api/src/main/java/com/epartner/services/TagService.java
+++ api/src/main/java/com/epartner/services/TagService.java
@@ -1,26 +1,20 @@
 package com.epartner.services;
 
-import com.epartner.converters.ProductConverter;
 import com.epartner.converters.TagConverter;
 import com.epartner.domain.Product;
 import com.epartner.domain.Tag;
 import com.epartner.repositories.ProductRepository;
 import com.epartner.repositories.TagRepository;
-import com.epartner.representations.ProductRepresentation;
 import com.epartner.representations.TagRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Created by martin on 24/09/16.
@@ -30,7 +24,6 @@ public class TagService {
 
     private TagConverter converter;
     private TagRepository repository;
-    private ProductConverter productConverter;
     private final ProductRepository productRepository;
 
     private final Integer MAX = 10;
@@ -40,12 +33,10 @@ public class TagService {
     public TagService(
         TagConverter tagConverter,
         TagRepository repository,
-        ProductConverter productConverter,
         ProductRepository productRepository){
 
         this.repository = repository;
         this.converter = tagConverter;
-        this.productConverter = productConverter;
         this.productRepository = productRepository;
     }
 
@@ -104,7 +95,28 @@ public class TagService {
                 .orElse(null);
     }
 
+    public void delete(TagRepresentation tagRepresentation) {
+        Tag stored = this.show(tagRepresentation.getId());
+
+        stored.getProducts().forEach(this::removeFromProduct);
+
+        this.repository.delete(stored);
+    }
+
     public TagRepresentation get(Long id) {
-        return this.converter.convert(Optional.ofNullable(this.repository.findOne(id)).orElseThrow(EntityExistsException::new));
+        return this.converter.convert(this.show(id));
+    }
+
+    private Tag show(Long id) {
+        return Optional.ofNullable(
+                this.repository.findOne(id))
+                .orElseThrow(EntityExistsException::new);
+    }
+
+    public void removeFromProduct(Product product) {
+        product.getTags().forEach(tag -> {
+            tag.getProducts().remove(product);
+            this.repository.save(tag);
+        });
     }
 }
